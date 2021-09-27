3a8e512051926e5ff10bd68135d518ad32467232
ChrisTimperley <christimperley@googlemail.com>
2021-09-27 17:39:37 -0400

    updated run script to set umask and mount results directory

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+HERE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 BENCHMARK_IMAGE="secbugs"
 VOLUME_HIFIX="hifix_opt"
 VOLUME_LLVM="llvm11_opt"
+RESULTS_DIR="${HERE_DIR}/../results"
 VOLUME_Z3="z3_opt"
+
+mkdir -p "${RESULTS_DIR}"
 
 docker run \
   --volume "${VOLUME_HIFIX}:/opt/hifix" \
   --volume "${VOLUME_LLVM}:/opt/llvm11" \
   --volume "${VOLUME_Z3}:/opt/z3" \
+  --env UMASK=0000 \
+  -v "${RESULTS_DIR}:/results" \
   --rm \
   -it \
   "${BENCHMARK_IMAGE}"
