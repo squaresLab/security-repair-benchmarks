9d03fbec2afd708206efe323f78b1f24875cc415
ChrisTimperley <christimperley@googlemail.com>
2021-10-28 14:54:33 -0400

    generated more of config files

--- scripts/generate-darjeeling-config.py
+++ scripts/generate-darjeeling-config.py
@@ -9,25 +9,15 @@ import yaml
 
 DESCRIPTION = "generates a Darjeeling config file for a given bug."
 
-LOCALIZATION_CONFIG = {
-    "type": "spectrum",
-    "metric": "tarantula",
-}
-OPTIMIZATIONS_CONFIG = {
-    "ignore-equivalent-insertions": "yes",
-    "ignore-dead-code": "yes",
-    "ignore-string-equivalent-snippets": "yes",
-}
-TRANSFORMATIONS_CONFIG = {
-    "schemas": [
-        {"type": "delete-statement"},
-        {"type": "replace-statement"},
-        {"type": "append-statement"},
+TESTS_CONFIG = {
+    "type": "shell",
+    "workdir": "FOO_BAR_BAZ",  # FIXME
+    "time-limit": 5,
+    "tests": [
+
     ],
 }
-ALGORITHM_CONFIG = {
-    "type": "exhaustive",
-}
+
 BUILD_INSTRUCTIONS = {
     "time-limit": 30,
     "environment": {
@@ -43,6 +33,76 @@ BUILD_INSTRUCTIONS = {
 }
 
 
+def generate_config(
+    seed: int,
+    threads: int,
+    max_candidates: int,
+) -> t.Dict[str, t.Any]:
+    config = {}
+    config["version"] = 1.0
+    config["seed"] = seed
+    config["threads"] = threads
+
+    config["algorithm"] = {
+        "type": "exhaustive",
+    }
+
+    config["transformations"] = {
+        "schemas": [
+            {"type": "delete-statement"},
+            {"type": "replace-statement"},
+            {"type": "append-statement"},
+        ],
+    }
+
+    config["resource-limits"] = {
+        "candidates": max_candidates,
+    }
+
+    config["localization"] = {
+        "type": "spectrum",
+        "metric": "tarantula",
+    }
+
+    config["optimizations"] = {
+        "ignore-equivalent-insertions": "yes",
+        "ignore-dead-code": "yes",
+        "ignore-string-equivalent-snippets": "yes",
+    }
+
+    # TODO program
+    # program:
+    #   image: darjeeling/example:zune
+    #   language: c
+    #   source-directory: /experiment/source
+    #   build-instructions:
+    #     time-limit: 15
+    #     environment:
+    #       REPAIR_TOOL: darjeeling
+    #       CC: /opt/llvm11/bin/clang
+    #       CXX: /opt/llvm11/bin/clang++
+    #     steps:
+    #       - REPAIR_TOOL=darjeeling ./prebuild
+    #       - REPAIR_TOOL=darjeeling ./build
+    #     steps-for-coverage:
+    #       - REPAIR_TOOL=darjeeling CFLAGS=- ./prebuild
+    #   tests:
+    #     type: genprog
+    #     workdir: /experiment
+    #     number-of-failing-tests: 4
+    #     number-of-passing-tests: 18
+    #     time-limit: 5
+
+    # TODO coverage
+    # coverage:
+    #   method:
+    #     type: gcov
+    #     files-to-instrument:
+    #       - zune.c
+
+    return config
+
+
 def generate_for_bug_file(bug_filename: str) -> None:
     bug_directory = os.path.dirname(bug_filename)
     output_filename = os.path.join(bug_directory, "repair.darjeeling.yml")
@@ -50,22 +110,20 @@ def generate_for_bug_file(bug_filename: str) -> None:
     if not os.path.exists(bug_filename):
         raise ValueError(f"bug.json file does not exist: {filename}")
 
+    # TODO turn these into command-line arguments to this script!
     seed = 0
     threads = 8
     max_candidates = 100
 
-    config = {
-        "version": "1.0",
-        "program": program_config,
-        "seed": seed,
-        "threads": threads,
-        "localization": LOCALIZATION_CONFIG,
-        "resource-limits": {
-            "candidates": max_candidates,
-        },
-    }
+    # FIXME
+    config = generate_config(
+        seed=seed,
+        threads=threads,
+        max_candidates=max_candidates,
+    )
 
-    raise NotImplementedError
+    with open(output_filename, "w") as fh:
+        yaml.dump(config, fh, default_flow_style=False)
 
 
 def generate_for_bug_directory(directory: str) -> None:
