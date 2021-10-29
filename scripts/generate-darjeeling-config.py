22a2803c2c817e23227ec4ee9443d36b2002b2dd
ChrisTimperley <christimperley@googlemail.com>
2021-10-29 14:17:20 -0400

    added more detail to auto-generated Darjeeling config

--- scripts/generate-darjeeling-config.py
+++ scripts/generate-darjeeling-config.py
@@ -34,9 +34,13 @@ BUILD_INSTRUCTIONS = {
 
 
 def generate_config(
+    program_name: str,
+    bug_name: str,
     seed: int,
     threads: int,
     max_candidates: int,
+    *,
+    source_directory: str = "/workspace/src",
 ) -> t.Dict[str, t.Any]:
     config = {}
     config["version"] = 1.0
@@ -70,22 +74,19 @@ def generate_config(
         "ignore-string-equivalent-snippets": "yes",
     }
 
-    # TODO program
+    # determine the name of the Docker image
+    docker_image_name = f"{program_name}-{bug_name}"
+
+    config["program"] = {
+        "image": docker_image_name,
+        "language": "c",
+        "source-directory": source_directory,
+        "build-instructions": BUILD_INSTRUCTIONS,
+        "tests": "FIXME",
+    }
+
+    # TODO add test config to program
     # program:
-    #   image: darjeeling/example:zune
-    #   language: c
-    #   source-directory: /experiment/source
-    #   build-instructions:
-    #     time-limit: 15
-    #     environment:
-    #       REPAIR_TOOL: darjeeling
-    #       CC: /opt/llvm11/bin/clang
-    #       CXX: /opt/llvm11/bin/clang++
-    #     steps:
-    #       - REPAIR_TOOL=darjeeling ./prebuild
-    #       - REPAIR_TOOL=darjeeling ./build
-    #     steps-for-coverage:
-    #       - REPAIR_TOOL=darjeeling CFLAGS=- ./prebuild
     #   tests:
     #     type: genprog
     #     workdir: /experiment
@@ -103,12 +104,23 @@ def generate_config(
     return config
 
 
-def generate_for_bug_file(bug_filename: str) -> None:
+def generate_for_bug_file(bug_filename: str) -> str:
     bug_directory = os.path.dirname(bug_filename)
     output_filename = os.path.join(bug_directory, "repair.darjeeling.yml")
 
     if not os.path.exists(bug_filename):
         raise ValueError(f"bug.json file does not exist: {filename}")
+
+    with open(bug_filename, "r") as fh:
+        bug_description = json.load(fh)
+
+    expected_fields = (
+        "subject",
+        "name",
+    )
+    for field_name in expected_fields:
+        if field_name not in bug_description:
+            raise ValueError(f"missing required property in bug.json: {field_name}")
 
     # TODO turn these into command-line arguments to this script!
     seed = 0
@@ -117,6 +129,8 @@ def generate_for_bug_file(bug_filename: str) -> None:
 
     # FIXME
     config = generate_config(
+        program_name=bug_description["subject"],
+        bug_name=bug_description["name"],
         seed=seed,
         threads=threads,
         max_candidates=max_candidates,
@@ -125,13 +139,15 @@ def generate_for_bug_file(bug_filename: str) -> None:
     with open(output_filename, "w") as fh:
         yaml.dump(config, fh, default_flow_style=False)
 
+    return output_filename
 
-def generate_for_bug_directory(directory: str) -> None:
+
+def generate_for_bug_directory(directory: str) -> str:
     if not os.path.exists(directory):
         raise ValueError(f"bug directory does not exist: {directory}")
 
     bug_filename = os.path.join(directory, "bug.json")
-    generate_for_bug_file(bug_filename)
+    return generate_for_bug_file(bug_filename)
 
 
 def main(args: t.Optional[t.Sequence[str]] = None) -> None:
@@ -142,7 +158,8 @@ def main(args: t.Optional[t.Sequence[str]] = None) -> None:
     )
 
     parsed_args = parser.parse_args(args)
-    generate_for_bug_directory(parsed_args.directory)
+    output_filename = generate_for_bug_directory(parsed_args.directory)
+    print(f"wrote Darjeeling config file: {output_filename}")
 
 
 if __name__ == "__main__":
