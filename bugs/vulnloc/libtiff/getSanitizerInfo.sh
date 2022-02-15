11b0bec76fb506a65e3fa2b4fa1d41a5205c9274
Tony <amespi22@asu.edu>
2022-02-15 10:32:49 -0700

    adding stuff to get column numbers

--- bugs/vulnloc/libtiff/getSanitizerInfo.sh
+++ bugs/vulnloc/libtiff/getSanitizerInfo.sh
@@ -9,20 +9,20 @@ CURRENT_BUG="$(basename "`pwd`")"
 ./clean
 
 # Builds using the address sanitizer
-CC=clang REPAIR_TOOL=none CFLAGS="-fsanitize=address -g -fno-omit-frame-pointer" CXXFLAGS="-fsanitize=address" ./prebuild
-CC=clang REPAIR_TOOL=none CFLAGS="-fsanitize=address -g -fno-omit-frame-pointer" CXXFLAGS="-fsanitize=address" ./build
-ASAN_OPTIONS=symbolize=0 ./test 2> "${STORAGE_FOLDER}/addressSan.txt"
+#CC=clang REPAIR_TOOL=none CFLAGS="-fsanitize=address -g -fno-omit-frame-pointer" CXXFLAGS="-fsanitize=address" ./prebuild
+#CC=clang REPAIR_TOOL=none CFLAGS="-fsanitize=address -g -fno-omit-frame-pointer" CXXFLAGS="-fsanitize=address" ./build
+#ASAN_OPTIONS=symbolize=0 ./test 2> "${STORAGE_FOLDER}/addressSan.txt"
 
 #builds using the memory sanitizer
-./clean
-CC=clang REPAIR_TOOL=none CFLAGS="-fsanitize=undefined -g -fno-omit-frame-pointer" CXXFLAGS="-fsanitize=undefined" ./prebuild
-CC=clang REPAIR_TOOL=none CFLAGS="-fsanitize=undefined -g -fno-omit-frame-pointer" CXXFLAGS="-fsanitize=undefined" ./build
-ASAN_OPTIONS=symbolize=0 ./test 2> "${STORAGE_FOLDER}/undefinedSan.txt"
+#./clean
+#CC=clang REPAIR_TOOL=none CFLAGS="-fsanitize=undefined -g -fno-omit-frame-pointer" CXXFLAGS="-fsanitize=undefined" ./prebuild
+#CC=clang REPAIR_TOOL=none CFLAGS="-fsanitize=undefined -g -fno-omit-frame-pointer" CXXFLAGS="-fsanitize=undefined" ./build
+#ASAN_OPTIONS=symbolize=0 ./test 2> "${STORAGE_FOLDER}/undefinedSan.txt"
 
 # Builds using both 
 ./clean
-CC=clang REPAIR_TOOL=none CFLAGS="-fsanitize=undefined,address -g -fno-omit-frame-pointer" CXXFLAGS="-fsanitize=undefined,address" ./prebuild
-CC=clang REPAIR_TOOL=none CFLAGS="-fsanitize=undefined,address -g -fno-omit-frame-pointer" CXXFLAGS="-fsanitize=undefined,address" ./build
+CC=clang-11 REPAIR_TOOL=none CFLAGS="-fsanitize=undefined,address -g -fno-omit-frame-pointer" CXXFLAGS="-fsanitize=undefined,address" ./prebuild
+CC=clang-11 REPAIR_TOOL=none CFLAGS="-fsanitize=undefined,address -g -fno-omit-frame-pointer" CXXFLAGS="-fsanitize=undefined,address" bear ./build
 ASAN_OPTIONS=symbolize=0 ./test 2> "${STORAGE_FOLDER}/bothSan.txt"
 
 exit 0
